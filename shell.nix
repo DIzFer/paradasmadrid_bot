d8f610740a95b3dac8874d7ff7d577ff122478cd
David Izquierdo <david@izquierdofernandez.com>
2018-05-20 19:17:31 +0200

    Añadido shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "paradasmadrid_bot";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    python3
+    python3Packages.python-telegram-bot
+  ];
+}
