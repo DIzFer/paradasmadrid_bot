08271e88efb3449afd8226c5d303a92242ba4012
David Izquierdo <david@izquierdofernandez.com>
2018-05-25 11:42:51 +0200

    respondemos a una parada de autobús con sus próximas llegadas

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import tokens
+from lib import obtener_autobuses, presentar_autobuses
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -26,11 +27,22 @@ Para usar este bot envia el codigo de la parada en la que estas.
 Se te devolveran los buses que estan por llegar, y lo que tardaran en llegar
 """)
 
+def obtener_parada(bot, update):
+    update.message.reply_text(
+        presentar_autobuses(
+            obtener_autobuses(
+                parada=update.message.text,
+                claves={"id_client": ID_CLIENT, "key": KEY}
+                )
+            )
+        )
 
 UPDATER = Updater(TOKEN)
 
 UPDATER.dispatcher.add_handler(CommandHandler('start', start))
 UPDATER.dispatcher.add_handler(CommandHandler('help', help))
+UPDATER.dispatcher.add_handler(CommandHandler('parada', obtener_parada))
+UPDATER.dispatcher.add_handler(MessageHandler(Filters.text, obtener_parada))
 
 UPDATER.start_polling()
 UPDATER.idle()
